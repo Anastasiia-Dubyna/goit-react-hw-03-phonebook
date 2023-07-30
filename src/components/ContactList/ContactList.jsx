d80d766cfd512ecbfdff0c2aeecdb0c6160a358d
Anastasiia-Dubyna <122117575+Anastasiia-Dubyna@users.noreply.github.com>
2023-07-30 14:52:10 +0300

    update

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+
+import { Contacts } from './Contacts';
+
+export function ContactList({ list, onDeleteItem }) {
+  return list.map(item => {
+    return (
+      <Contacts
+        key={item.id}
+        contact={item}
+        onDelItem={() => onDeleteItem(item.id)}
+      />
+    );
+  });
+}
+
+ContactList.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.object),
+  onDelItem: PropTypes.func,
+};
